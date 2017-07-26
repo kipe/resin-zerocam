8f326725ad1142fe5c4fc3266e3c3a77da5a6537
Kimmo Huoman <kipenroskaposti@gmail.com>
2017-07-26 21:24:25 +0300

    Add encode to GET /api/capture

--- src/http_server.py
+++ src/http_server.py
@@ -80,7 +80,7 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
             self.send_header('Content-Type', 'application/json')
             self.send_header('Content-Length', len(content))
             self.end_headers()
-            self.wfile.write(content)
+            self.wfile.write(content.encode('utf-8'))
         else:
             self.send_error(404)
             self.end_headers()
